37e96f5c8ded2d5775019423d1bc94b82a8bc539
Shing Liu <eryar@163.com>
2016-05-27 18:37:48 +0800

    occ 6.9 shader path
    since 6.9.0 release, OCCT uses GLSL programs for basic functionality,
    this is done by the CSF_ShadersDirectory variable.

--- main.cpp
+++ main.cpp
@@ -13,8 +13,19 @@
 #include "occQt.h"
 #include <QApplication>
 
+#include <OSD_Environment.hxx>
+
 int main(int argc, char *argv[])
 {
+    // refer to: http://www.opencascade.com/content/error-after-upgrading-690-regarding-csfshadersdirectory-and-casroot-environment-variables
+    // since 6.9.0 release, OCCT uses GLSL programs for basic functionality 
+    // when provided by driver (not just custom GLSL programs explicitly specified by user).
+    // There is no option to disable this functionality, thus your application should 
+    // configure CSF_ShadersDirectory to proper location in addition to other OCCT resources.
+    // We will take to update description of CSF_ShadersDirectory variable in OCCT documentation.
+    OSD_Environment aShaderEnv("CSF_ShadersDirectory", "D:/OpenCASCADE6.9.0/opencascade-6.9.0/src/Shaders");
+    aShaderEnv.Build();
+
     QApplication a(argc, argv);
 
     occQt w;
